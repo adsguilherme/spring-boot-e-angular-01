495b99f18149ee6b5b27c21a9506d1c44ea9cc2d
gscode <adm.guilhermedesousa@gmail.com>
2025-12-12 17:28:42 -0300

    configurando camada de repositorio

--- api/src/main/java/com/br/projeto/api/modelo/Cliente.java
+++ api/src/main/java/com/br/projeto/api/modelo/Cliente.java
@@ -1,4 +1,20 @@
 package com.br.projeto.api.modelo;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clientes") // Se não passar o name, vai pegar o nome da classe (ficando Cliente).
+@Getter
+@Setter
 public class Cliente {
+
+    @Id // Annotation para dizer que código será nossa chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Annotation para dizer que o código será gerado automaticamente
+    private long codigo;
+    private String nome;
+    private int idade;
+    private String cidade;
+
 }
--- api/src/main/java/com/br/projeto/api/repositorio/Repositorio.java
+++ api/src/main/java/com/br/projeto/api/repositorio/Repositorio.java
@@ -1,4 +1,11 @@
 package com.br.projeto.api.repositorio;
 
-public interface Repositorio {
+import com.br.projeto.api.modelo.Cliente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Repositorio extends CrudRepository<Cliente, Long> { // Cliente é nossa entidade (modelo) e o tipo de dado da chave primária.
+
+
 }
