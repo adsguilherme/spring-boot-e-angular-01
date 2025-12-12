f0289e52e6c728c33ce083c8232a1266be6ccf09
gscode <adm.guilhermedesousa@gmail.com>
2025-12-12 17:50:01 -0300

    implementando annotation @Autowired

--- api/src/main/java/com/br/projeto/api/controle/Controle.java
+++ api/src/main/java/com/br/projeto/api/controle/Controle.java
@@ -1,10 +1,15 @@
 package com.br.projeto.api.controle;
 
+import com.br.projeto.api.repositorio.Repositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controle {
+
+    @Autowired
+    private Repositorio acao;
 
     @GetMapping("/")
     public String teste(){
