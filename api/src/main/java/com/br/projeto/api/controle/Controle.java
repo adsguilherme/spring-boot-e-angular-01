95c5180105021d70a0e0ce2c1c3bbc7ffb6ec7f7
gscode <adm.guilhermedesousa@gmail.com>
2025-12-12 13:42:45 -0300

    configurando camada de controle

--- api/src/main/java/com/br/projeto/api/controle/Controle.java
+++ api/src/main/java/com/br/projeto/api/controle/Controle.java
@@ -1,4 +1,13 @@
 package com.br.projeto.api.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controle {
+
+    @GetMapping("/")
+    public String teste(){
+        return "Hello World!";
+    }
 }
