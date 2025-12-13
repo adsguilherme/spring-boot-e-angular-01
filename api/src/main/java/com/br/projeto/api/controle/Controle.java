e8b25ae18bc42587f0a0da4eddc92c2af4a212d3
gscode <adm.guilhermedesousa@gmail.com>
2025-12-13 15:36:05 -0300

    implementando rota de cadastro - POST

--- api/src/main/java/com/br/projeto/api/controle/Controle.java
+++ api/src/main/java/com/br/projeto/api/controle/Controle.java
@@ -1,8 +1,11 @@
 package com.br.projeto.api.controle;
 
+import com.br.projeto.api.modelo.Cliente;
 import com.br.projeto.api.repositorio.Repositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +14,13 @@ public class Controle {
     @Autowired
     private Repositorio acao;
 
-    @GetMapping("/")
-    public String teste(){
-        return "Hello World!";
+    @PostMapping("/cadastrar")
+    public Cliente cadastrar(@RequestBody Cliente cliente) {
+        return acao.save(cliente);
     }
+
+//    @GetMapping("/")
+//    public String teste(){
+//        return "Hello World!";
+//    }
 }
--- api/src/main/java/com/br/projeto/api/repositorio/Repositorio.java
+++ api/src/main/java/com/br/projeto/api/repositorio/Repositorio.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Repositorio extends CrudRepository<Cliente, Long> { // Cliente é nossa entidade (modelo) e o tipo de dado da chave primária.
-
+public interface Repositorio extends CrudRepository<Cliente, Long> {
 
 }
